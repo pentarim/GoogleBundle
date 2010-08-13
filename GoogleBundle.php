3ac34132f47083885e56c780af556140cd655b4d
Bulat Shakirzyanov <mallluhuct@gmail.com>
2010-08-13 11:28:03 -0400

    upgraded DI extension registration

--- GoogleBundle.php
+++ GoogleBundle.php
@@ -3,21 +3,5 @@
 namespace Bundle\GoogleBundle;
 
 use Symfony\Framework\Bundle\Bundle as BaseBundle;
-use Symfony\Components\DependencyInjection\ContainerInterface;
-use Symfony\Components\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Components\DependencyInjection\ContainerBuilder;
-use Bundle\GoogleBundle\DependencyInjection\GoogleExtension;
 
-class GoogleBundle extends BaseBundle {
-    
-	/**
-     * Customizes the Container instance.
-     *
-     * @param Symfony\Components\DependencyInjection\ParameterBag\ParameterBagInterface $parameterBag A ParameterBagInterface instance
-     * @return Symfony\Components\DependencyInjection\BuilderConfiguration A BuilderConfiguration instance
-     */
-    public function buildContainer()
-    {
-        ContainerBuilder::registerExtension(new GoogleExtension());
-    }
-}
+class GoogleBundle extends BaseBundle {}
